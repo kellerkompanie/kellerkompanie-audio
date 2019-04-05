2f96560804464f51fa820fe305ddc1c4821a5cf2
Schwaggot <tom.ryan@posteo.de>
2019-04-05 21:29:49 +0200

    added main

---
+++ addons/main/config.cpp
@@ -0,0 +1,34 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "A3_Data_F_Sams_Loadorder",
+            "A3_Data_F_Mod_Loadorder",
+            "cba_ui",
+            "cba_xeh",
+            "cba_jr",
+            "ace_main"
+        };
+        author = "Schwaggot";
+        url = CSTRING(URL);
+        VERSION_CONFIG;
+    };
+};
+
+class CfgMods {
+    class PREFIX {
+        dir = "@keko_audio";
+        name = "Kellerkompanie Audio Mods";
+        picture = "A3\Ui_f\data\Logos\arma3_expansion_alpha_ca";
+        hidePicture = "true";
+        hideName = "true";
+        actionName = "Website";
+        action = CSTRING(URL);
+        description = "Issue Tracker: https://github.com/kellerkompanie/kellerkompanie-audio/issues";
+    };
+};
