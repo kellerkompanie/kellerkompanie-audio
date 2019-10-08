d6c94bd699b549893557ea9c84a1b51116b900ab
Schwaggot <tom.ryan@posteo.de>
2019-10-08 18:53:18 +0200

    fixed error when loading fallout

--- addons/audio_fallout/script_component.hpp
+++ addons/audio_fallout/script_component.hpp
@@ -1,6 +1,6 @@
 #define COMPONENT audio_fallout
 #define COMPONENT_BEAUTIFIED AudioFallout
-#include "\x\keko_audio\addons\main\script_mod.hpp"
+#include "\x\keko\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
@@ -14,4 +14,4 @@
     #define DEBUG_SETTINGS DEBUG_SETTINGS_AUDIO_FALLOUT
 #endif
 
-#include "\x\keko_audio\addons\main\script_macros.hpp"
+#include "\x\keko\addons\main\script_macros.hpp"
